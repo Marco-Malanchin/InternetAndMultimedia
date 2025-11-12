16f3306bd39174062ece795850bccc66f633c209
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2025-11-12 18:25:00 +0100

    Shazam da finire

--- MultimediaLab7/fun_windowing.m
+++ MultimediaLab7/fun_windowing.m
@@ -1,10 +1,35 @@
 function [W] = fun_windowing(y,windowshape,step)
-[a, b] = size(y);
-M = windowshape(0);
-rect = ones(M,1);
-k = ceil(a/step);
-W(M,k);
-for i = 1:k
+y = y(:);                 % forza il segnale a colonna
+[a, b] = size(y); %prendo la dimensione della traccia
+M = windowshape(1);%prendo il valore della finestra
+rect = ones(M,1);%creo la finestra
+k = ceil(a/step); %capisco di quante colonne è composta la matrice W
+W = zeros(M,k); %creo la matrice W
+startS = 1;
+endS = startS + M - 1; %creo gli indici di campionamento
+  for i = 1:k
+        % Se la finestra va oltre il segnale, applica zero-padding
+        if endS > a
+            segment = [y(startS:end); zeros(endS - a, 1)];
+        else
+            segment = y(startS:endS);
+        end
+
+         % Se segment è più corto o più lungo di M, sistemalo
+        if length(segment) < M
+            segment = [segment; zeros(M - length(segment), 1)];
+        elseif length(segment) > M
+            segment = segment(1:M);
+        end
+
+        % Applica la finestra rettangolare
+        W(:, i) = segment .* rect;
+
+        % Aggiorna indici
+        startS = startS + step;
+        endS = startS + M - 1;
+   end
+end
     
 
 
